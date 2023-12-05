9da1f76e1173226c8a8d5d2104a220c7d96fdbb0
Lee Sehyeon <lifethis21@kookmin.ac.kr>
2023-12-05 16:08:22 +0900

    MST kruskal 구현 완료

---
+++ week13/mst_kruskal.c
@@ -0,0 +1,156 @@
+/*
+ * mst_kruskal.c
+ * 소프트웨어학부 20213043 이세현
+ * test case input
+   3
+   10
+   1 3 2 9 3 8 4 5
+   2 3 1 9 5 7 6 9
+   3 5 1 8 4 3 5 2 7 3 8 5
+   4 3 1 5 3 3 8 7
+   5 4 2 7 3 2 6 5 7 4
+   6 5 2 9 5 5 7 6 9 9 10 8
+   7 5 3 3 5 4 6 6 8 4 9 7
+   8 4 3 5 4 7 7 4 9 4
+   9 4 6 9 7 7 8 4 10 3
+   10 2 6 8 9 3
+   5
+   1 3 2 1 4 8 5 5
+   2 2 1 1 3 6
+   3 1 2 6
+   4 1 1 8
+   5 1 1 5
+   5
+   1 4 2 20 3 12 4 12 5 8
+   2 3 1 20 3 17 4 5
+   3 3 1 12 2 17 5 6
+   4 3 1 12 2 5 5 5
+   5 3 1 8 3 6 4 5
+ * test case output
+   36
+   20
+   24
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+#define MAX_N 1001
+
+int edges[MAX_N][MAX_N];
+int parent[MAX_N];
+int size[MAX_N];
+
+typedef struct{
+    int v, w;
+    int weight;
+} neighbor;
+
+int kruskal(int n);
+
+void init_graph(int n) {
+    for(int i = 1; i <= n; i++) {
+        parent[i] = i;
+        size[i] = 1;
+        for(int j = 1; j <= n; j++) {
+            edges[i][j] = INT_MAX;
+        }
+    }
+}
+
+int main() {
+    int numTestCases;
+    scanf("%d", &numTestCases);
+    while (numTestCases--) {
+        int n;
+        scanf("%d", &n);
+        init_graph(n);
+        for(int i = 1; i <= n; i++) {
+            int k, m;
+            scanf("%d %d", &k, &m);
+            while(m--) {
+                int v, w;
+                scanf("%d %d", &v, &w);
+                edges[k][v] = w;
+            }
+        }
+        int answer = kruskal(n);
+        printf("%d\n", answer);
+    }
+}
+
+int compare(const void *a, const void *b) {
+    return ((neighbor*)a)->weight - ((neighbor*)b)->weight;
+}
+
+int find(int i) {
+    int p;
+    int root = i;
+    while(parent[root] != root) {
+        root = parent[root];
+    }
+    while(parent[i] != root) {
+        p = parent[i];
+        parent[i] = root;
+        i = p;
+    }
+    return root;
+}
+
+void merge(int p, int q) {
+    int i = find(p);
+    int j = find(q);
+    if(i != j) {
+        if(size[i] < size[j]) {
+            parent[i] = j;
+            size[j] += size[i];
+        }
+        else {
+            parent[j] = i;
+            size[i] += size[j];
+        }
+    }
+}
+
+int equal(int p, int q) {
+    int i = find(p);
+    int j = find(q);
+    if(i == j) return 1;
+    return 0;
+}
+
+int kruskal(int n) {
+    int answer = 0;
+    int edgeCount = 0;
+
+    neighbor *sortedEdges = malloc(sizeof(neighbor) * n * n);
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            if (edges[i][j] < INT_MAX) {
+                sortedEdges[edgeCount].v = i;
+                sortedEdges[edgeCount].w = j;
+                sortedEdges[edgeCount].weight = edges[i][j];
+                edgeCount++;
+            }
+        }
+    }
+
+    qsort(sortedEdges, edgeCount, sizeof(neighbor), compare);
+
+    for(int i = 0; i < edgeCount; i++) {
+        int u = sortedEdges[i].v;
+        int v = sortedEdges[i].w;
+
+        int p = find(u);
+        int q = find(v);
+
+        if(!equal(p, q)) {
+            merge(p, q);
+            answer += sortedEdges[i].weight;
+        }
+    }
+    free(sortedEdges);
+    return answer;
+}
